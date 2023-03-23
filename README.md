bd3145fd32c4cbd8108fff92024190a9d07cf833
Persie0 <hashcod3@protonmail.com>
2023-03-23 20:41:47 +0100

    f

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 This bot send you a message if the CNN fear and greed index is below 30 (fear).
 ## How to set up
 1. Create a bot with the BotFather
-2. Add your bot token and your chat id to the github secrets
+2. Get your chat id by sending a message to the bot and then go to https://api.telegram.org/bot<yourtoken>/getUpdates
+3. Add your bot token and your chat id to the github secrets
 one with the name `TELEGRAM_BOT_TOKEN` and one with the name `TELEGRAM_CHAT_ID`
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-import os
 import telegram
 import fear_and_greed
+import asyncio
 
 # Set up the Telegram bot
-bot_token = os.environ['TELEGRAM_BOT_TOKEN']
-chat_id = os.environ['TELEGRAM_CHAT_ID']
+bot_token = "6223994870:AAHX7P4Pk3VNc7rnqEBeOzes28fBmISlHBs"
+chat_id = 576668948
 
-# Create a bot instance
-bot = telegram.Bot(token=bot_token)
+async def notify(value:float):
+    if value < 30:
+        # Send a notification if the value is below 30
+        message = f"The Fear and Greed Index is {format(round(value, 2))}" 
+        await bot.send_message(chat_id=chat_id, text=message)
 
-# Get the Fear and Greed Index value
-value = fear_and_greed.get().value
+    print("Sent a notification")
 
-if value < 30:
-    # Send a notification if the value is below 30
-    message = f"The Fear and Greed Index is {value}. Time to buy some crypto!"
-    bot.send_message(chat_id=chat_id, text=message)
-else:
-    # Otherwise, just print the value to the console
-    #print(f"The Fear and Greed Index is {value}.")
-    message = f"The Fear and Greed Index is {value}."
-    bot.send_message(chat_id=chat_id, text=message)
+if __name__ == "__main__":
+    # Create a bot instance
+    bot = telegram.Bot(token=bot_token)
+    # Get the Fear and Greed Index value
+    value = fear_and_greed.get().value
+    asyncio.run(notify(value))
